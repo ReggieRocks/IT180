62d118e4825d8544eeac0d8ca4bd740c3021dd0b
RR <sschmi2@ilstu.edu>
2025-09-05 19:42:23 -0500

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -3,7 +3,7 @@
 using namespace std; // lets you write cout instead of std::cout
 
 //FORWARD DECLARATIONS:
-int mean(int arr[], int size); 
+double mean(int arr[], int size); 
 int findMin(int arr[], int size);
 int findMax(int arr[], int size);
 
@@ -26,30 +26,30 @@ int main() {
     // Find Mean:
     double avg = mean(arr, SIZE);
     cout << "The mean is: " << avg << endl;
-    return 0;
     
-    double min = findMin(arr, SIZE);
+    int min = findMin(arr, SIZE);
     cout << "The min is: " << min;
     
-    double max = findMin(arr, SIZE);
+    int max = findMax(arr, SIZE);
     cout << "The max is: " << max;
     
+    return 0; // return 0 if nothing found
 }
 
 // =========FUNCTIONS==========
+    // Find Mean Function:
+    double mean(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        sum += arr[i];
+    return static_cast<double>(sum) / size;
+}
     /*
     Part D: Function finds lowest value of array.
     Write a function that finds the lowest value of the declared array.
     Returns the lowest value found to main to be displayed. 
     Use forward declaration for this function. (5 points)
     */
-    int mean(int arr[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; i++)
-        sum += arr[i];
-    return static_cast<double>(sum) / size;
-}
-    
     int findMin(int arr[], int size) {
         int min = arr[0];
         for (int i = 1; i < size; i++) {
@@ -71,3 +71,27 @@ int main() {
     }
  // end
 
+ /*
+ ====OUTPUT======
+ Enter 15 numbers:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+You entered:
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
+The mean is: 8
+The min is: 1The max is: 15
+
+=== Code Execution Successful ===
